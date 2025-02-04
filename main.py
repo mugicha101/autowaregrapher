3686b866ec3b6c27983077f2fe412d54b29d0562
Alexander Yoshida <alexander.yoshida@gmail.com>
2025-02-04 13:43:38 -0500

    unify node and topic helpers

--- main.py
+++ main.py
@@ -4,42 +4,39 @@ from ros2node.api import get_node_names
 from ros2topic.api import get_topic_names
 from collections import defaultdict
 
-# to differentiate nodes from topics, use "node:" and "topic:" prefixes
+# to differentiate nodes from topics, use "node:" and "topic:" prefixes (nodes/topics generalized to comps)
 
 # helpers to get info from node graph
 
 link_node = NodeStrategy("/clock")
 
-node_names = get_node_names(node=link_node, include_hidden_nodes=True)
-topic_names = get_topic_names(node=link_node, include_hidden_topics=True)
+node_names = [ f"node:{name.full_name}" for name in get_node_names(node=link_node, include_hidden_nodes=True) ]
+topic_names = [ f"topic:{name}" for name in get_topic_names(node=link_node, include_hidden_topics=True) ]
+comp_names = node_names + topic_names
 
 print(node_names)
 print(topic_names)
-  
-def get_node_subs(node_name):
-  return [ "topic:" + info.name for info in get_subscriber_info(node=link_node, remote_node_name=node_name) ]
 
-def get_node_pubs(node_name):
-  return [ "topic:" + info.name for info in get_publisher_info(node=link_node, remote_node_name=node_name) ]
+def get_subs(comp_name):
+  type, name = comp_name.split(":")
+  if type == "node": return [ f"topic:{info.name}" for info in get_subscriber_info(node=link_node, remote_node_name=name) ]
+  if type == "topic": return [ f"node:{info.full_name}" for info in link_node.get_publishers_info_by_topic(name) ]
+  return None
 
-def get_topic_pubs(topic_name):
-  return [ "node:" + info.name for info in link_node.get_publishers_info_by_topic(topic_name) ]
-
-def get_topic_subs(topic_name):
-  return [ "node:" + info.name for info in link_node.get_subscriptions_info_by_topic(topic_name) ]
+def get_pubs(comp_name):
+  type, name = comp_name.split(":")
+  if type == "node": return [ f"topic:{info.name}" for info in get_publisher_info(node=link_node, remote_node_name=name) ]
+  if type == "topic": return [ f"node:{info.full_name}" for info in link_node.get_publishers_info_by_topic(name) ]
+  return None
 
 # construct graph
 
-pubs = defaultdict(list) # publishers of a topic/node
-subs = defaultdict(list) # subscribers of a topic/node
+pubs = defaultdict(list) # publishers of a comp
+subs = defaultdict(list) # subscribers of a comp
 
-for name in topic_names:
-  subs[name] = get_topic_subs(name)
-  pubs[name] = get_topic_pubs(name)
-
-for name in node_names:
-  subs[name] = get_node_subs(name)
-  pubs[name] = get_node_pubs(name)
+for name in comp_names:
+  subs[name] = get_subs(name)
+  pubs[name] = get_pubs(name)
 
 # find chains
 
